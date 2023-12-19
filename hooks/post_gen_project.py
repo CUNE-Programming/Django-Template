d97358218374e18d9f3153a739fbe2030d310a61
Ian Kollipara <ian.kollipara@cune.edu>
2023-12-19 07:47:46 -0600

    Add license and update post generation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,19 +1,25 @@
+""" Post generation hook for cookiecutter template. """ ""
+
+# Imports
 from pathlib import Path
 from os import system
 from json import load
 from shutil import which
 from platform import system as platform
 
+# Cookiecutter variables
 stack = "{{ cookiecutter.stack }}"
 css = "{{ cookiecutter.css }}"
 init_git: bool = {{cookiecutter.init_git}}
 install_packages: bool = {{cookiecutter.install_packages}}
 install_extensions: bool = {{cookiecutter.install_extensions}}
 
+# Create .env file
 Path(".env").write_text(
     "DEBUG=on\n" 'SECRET_KEY="secret"\n' "DATABASE_URL=sqlite:///dev.sqlite\n"
 )
 
+# Handle stack updates
 match stack:
     case "THAD":
         Path("resources/js").mkdir()
@@ -22,6 +28,7 @@ match stack:
         Path("resources/components").mkdir()
         Path("resources/hooks").mkdir()
 
+# Handle CSS updates
 match css:
     case "tailwind":
         Path("resources/scss/index.scss").unlink()
@@ -32,6 +39,7 @@ match css:
         Path("resources/css/tailwind.css").unlink()
         Path("resources/css").rmdir()
 
+# Install things if chosen
 if install_packages:
     if which("npm") is None:
         print("NPM not found. Skipping package installation.")
@@ -59,15 +67,17 @@ if init_git:
     else:
         system("./.venv/Scripts/pre-commit.exe install")
 
+    system("git add .")
+
     while (first_commit := input("Create first commit? [y/n] ").lower()) not in [
         "y",
         "n",
     ]:
         print("Please enter 'y' or 'n'")
     if first_commit == "y":
-        system("git add .")
         system('git commit -m ":rocket: Initial commit"')
 
 else:
     Path(".gitignore").unlink()
+    Path(".gitattributes").unlink()
     Path(".pre-commit-config.yaml").unlink()
